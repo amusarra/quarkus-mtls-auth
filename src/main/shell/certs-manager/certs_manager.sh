42c14432c1cdf464dfeaf011e370dbd2cad37c98
amusarra <antonio.musarra@gmail.com>
2024-09-04 00:22:48 +0200

    Removed duplicate code

--- src/main/shell/certs-manager/certs_manager.sh
+++ src/main/shell/certs-manager/certs_manager.sh
@@ -46,13 +46,6 @@
 # Source common functions
 source _common.sh
 
-# Define colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
-
 # Check if OpenSSL is installed and the version is correct
 check_openssl_installed
 check_openssl_version
