ef35eaeb9b30dd900ccaddf53b0ed3b02c420385
amusarra <antonio.musarra@gmail.com>
2024-09-08 10:41:47 +0200

    Convert SecurityErrorResponse class to Record Type

--- src/main/java/it/dontesta/quarkus/tls/auth/ws/error/mapper/SecurityErrorResponse.java
+++ src/main/java/it/dontesta/quarkus/tls/auth/ws/error/mapper/SecurityErrorResponse.java
@@ -6,34 +6,11 @@
 package it.dontesta.quarkus.tls.auth.ws.error.mapper;
 
 /**
- * The ErrorResponse class represents the error response returned by the SecurityExceptionMapper.
+ * The ErrorResponse record represents the error response returned by the SecurityExceptionMapper.
  *
- * <p>This class is used to encapsulate the error response returned by the SecurityExceptionMapper.
+ * <p>This record is used to encapsulate the error response returned by the SecurityExceptionMapper.
  *
  * @author Antonio Musarra
  */
-public class SecurityErrorResponse {
-  private int statusCode;
-  private String message;
-
-  public SecurityErrorResponse(int statusCode, String message) {
-    this.statusCode = statusCode;
-    this.message = message;
-  }
-
-  public int getStatusCode() {
-    return statusCode;
-  }
-
-  public void setStatusCode(int statusCode) {
-    this.statusCode = statusCode;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
+public record SecurityErrorResponse(int statusCode, String message) {
 }
